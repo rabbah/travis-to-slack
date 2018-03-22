324bc446a5d33e4837838408b2ec741f1ff5e6ab
David Grove <groved@us.ibm.com>
2018-03-21 21:04:22 -0400

    working, but very fragile logic for whiskbot to support subscribe
    Messages of exactly the form "subscribe travis Jane Doe" on a DM channel
    with @whiskbot will now result in addAuthor being triggered.

--- whiskbot.js
+++ whiskbot.js
@@ -16,7 +16,61 @@
 
 const prefix = 'travis2slack'
 
+const slackConfig = {
+    token: process.env['SLACK_TOKEN'],
+    username: 'whiskbot',
+    url: 'https://slack.com/api/chat.postMessage'
+}
 
-composer.sequence(
-    `/whisk.system/utils/echo`
+if (slackConfig.token === undefined) {
+    console.error('SLACK_TOKEN required in environment.')
+    process.exit(-1)
+}
+
+function confusedMessage(args) {
+    const user = args.payload.user
+    const text = args.payload.text
+    const message = `Sorry <@${user}>. I did not understand "${text}"\nTry saying \'subscribe travis "Git name"\' instead`
+
+    return Object.assign(sc, { channel: '@' + user, text: message })
+}
+
+function victoryMessage(args) {
+    const user = args.payload.user
+    const text = args.payload.text
+    const message = `Rock me amadeus!`
+
+    return Object.assign(sc, { channel: '@' + user, text: message })
+}
+
+function parseAsSubscribe(args) {
+    const user = args.payload.user;
+    const text = args.payload.text;
+    const command = 'subscribe travis '
+
+    if (!text.startsWith(command)) {
+        return { error: "Is not a subscribe message" }
+    }
+    const name = text.slice(command.length)
+
+    return { name: name, userID: user }
+}
+
+
+composer.let({ sc: slackConfig },
+    composer.sequence(
+        `/whisk.system/utils/echo`,
+        composer.let({ input: {} },
+            p => { input = p; return p },
+            composer.try(
+                composer.sequence(
+                    parseAsSubscribe,
+                    `${prefix}/addAuthor`),
+                composer.sequence(
+                    _ => Object.assign(input, { slackConfig: sc }),
+                    confusedMessage,
+                    `/whisk.system/slack/post`
+                ))
+        )
+    )
 )
