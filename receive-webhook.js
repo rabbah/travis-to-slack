4de418cdae1ec534609e9c3b091f65f0ae13374e
David Grove <dgrove-oss@users.noreply.github.com>
2018-03-23 17:08:20 -0400

    Slackbot backend (#4)
    * basic forwarding of slack events to cloud functions working
    
    * working, but very fragile logic for whiskbot to support subscribe
    
    Messages of exactly the form "subscribe travis Jane Doe" on a DM channel
    with @whiskbot will now result in addAuthor being triggered.
    
    * Rework to reduce code duplication and use Slack /commands for subscribe/unsubscribe
    
    * Don't allow overwrites; if a subscription for a name already exists report an error.
    
    * add visualization of addSubscription showing nested try/catch structure
    
    Signed-off-by: David Grove <groved@us.ibm.com>

--- receive-webhook.js
+++ receive-webhook.js
@@ -1,33 +1,60 @@
 var openwhisk = require('openwhisk')
 
 function main(args) {
-    const wsk = openwhisk({ignore_certs: args.$ignore_certs || false})
+    const wsk = openwhisk({ ignore_certs: args.$ignore_certs || false })
+    var payload = undefined
+    var initial_response = { text: 'ok' }
 
-    if (args.payload) {
+    if (args["$eventSource"] == 'travis') {
+        console.log('Recevied event from travis')
         try {
             payload = JSON.parse(args.payload)
-            return new Promise(function(resolve, reject) {
-                wsk.actions
-                    .invoke({
-                        actionName: args["$actionName"],
-                        params: payload,
-                        blocking: false
-                    })
-                    .then(id => {
-                        console.log(id)
-                        resolve()
-                    })
-                    .catch(e => {
-                        console.log('error:', e)
-                        reject()
-                    })
-            })
         } catch (e) {
-            console.log('exception', e)
+            console.log('Exception parsing args.payload', e)
             return
         }
+    } else if (args["$eventSource"] == 'slack') {
+        console.log('Received event from slack')
+        if (args.token != args.expected_token) {
+            console.log('Dropping slack event with invalid token')
+            console.log(args)
+            return
+        }
+
+        // prevent secret from propagating downstream
+        args.token = '<redacted slack token>'
+        args.expected_token = '<redacted slack token>'
+
+        // Slack url_verification protocol; return challenge value
+        if (args.type == 'url_verification') {
+            console.log('Received url_verification request')
+            return { body: args.challenge }
+        }
+
+        // Some other slack event; flow args through to target action
+        payload = args
+        if (args.command) {
+            initial_response = { text: 'Starting execution of ' + args.command }
+        }
+
     } else {
-        console.log('no payload')
-        return
+        console.log('Received event from unexpected source ' + args["$eventSource"])
     }
+
+    return new Promise(function (resolve, reject) {
+        wsk.actions
+            .invoke({
+                actionName: args["$actionName"],
+                params: payload,
+                blocking: false
+            })
+            .then(id => {
+                console.log(id)
+                resolve(initial_response)
+            })
+            .catch(e => {
+                console.log('error:', e)
+                reject()
+            })
+    })
 }
